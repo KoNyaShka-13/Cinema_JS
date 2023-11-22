a228c8640a6701893478e5bc0b397f5f6720b3b2
KoNyaShKa <ggiziawpgg@gmail.com>
2023-11-22 10:20:14 +0300

    job
    Небольшая практика перед изучением React

--- project/js/script.js
+++ project/js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -23,4 +9,27 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+const adv = document.querySelectorAll('.promo__adv img');//В данном случае у нас коллекция, мы не можем из-за этого исполььзовать метод remove, нужно будет удалять по отдельности
+
+const poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movielist = document.querySelector('.promo__interactive-list');//Берем сам список, а не псевдоэлемент из элементов списка для более удобного исползования
+
+adv.forEach(item => {//Через функцию удаляем объекты, что находились в массиве
+    item.remove();
+});
+
+genre.textContent = 'драма';//Изменили текст
+
+poster.style.backgroundImage = "url(img/bg.jpg)";
+movielist.innerHTML = '';//Очищаем список
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {//Делаем переборку и пример того, как записать ответ
+    movielist.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
 
