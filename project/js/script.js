50c39f23d28a040663abaf10a2c45629b2ca6736
KoNyaShKa <ggiziawpgg@gmail.com>
2023-11-27 09:01:14 +0300

    Доработка
    Автоматическая сортировка, добавление и удаление элементов списка и т.п.

--- project/js/script.js
+++ project/js/script.js
@@ -1,35 +1,95 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {//После того, как дом дерево загрузилось, начинаем зугрузку остального, чтобы все загрузилось без ошибок
 
-const adv = document.querySelectorAll('.promo__adv img');//В данном случае у нас коллекция, мы не можем из-за этого исполььзовать метод remove, нужно будет удалять по отдельности
+    const movieDB = {
+        movies: [
+            "логан",
+            "лига справедливости",
+            "ла-ла лэнд",
+            "одержимость",
+            "скотт Пилигрим против..."
+        ]
+    };
 
-const poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movielist = document.querySelector('.promo__interactive-list');//Берем сам список, а не псевдоэлемент из элементов списка для более удобного исползования
+    const adv = document.querySelectorAll('.promo__adv img'),//В данном случае у нас коллекция, мы не можем из-за этого исполььзовать метод remove, нужно будет удалять по отдельности
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),//Берем сам список, а не псевдоэлемент из элементов списка для более удобного исползования
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
 
-adv.forEach(item => {//Через функцию удаляем объекты, что находились в массиве
-    item.remove();
-});
+    addForm.addEventListener('submit', (event) => {//Отменяем нормальную работу браузера, чтобы страница при обновлении данных не бновлялась
+        event.preventDefault();
 
-genre.textContent = 'драма';//Изменили текст
 
-poster.style.backgroundImage = "url(img/bg.jpg)";
-movielist.innerHTML = '';//Очищаем список
-movieDB.movies.sort();
-movieDB.movies.forEach((film, i) => {//Делаем переборку и пример того, как записать ответ
-    movielist.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;//Обрезаем и добавляем ...
+            }
+
+            if (favorite) {
+                console.log("Добавляем любимый фильм");
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {//Через функцию удаляем объекты, что находились в массиве
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        genre.textContent = 'драма';//Изменили текст
+
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+
+//Добавляем / удаляем и сортируем новые элементы
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArr(films);//Добавляем сюда для того, чтобы сразу все сортировалось  с вызовом функции
+    
+        films.forEach((film, i) => {//Делаем переборку и пример того, как записать ответ
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);//Используем рекурсию для обновления нумерации
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 });
 
